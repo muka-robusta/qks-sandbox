e485f0cd74df7f44a052d84d5bb653938a4dc3a9
Illia Savenko <illiafun@yandex.ua>
2021-08-08 15:13:17 +0300

    Initial commit

---
+++ src/main/java/com/onetwostory/Greeter.java
@@ -0,0 +1,37 @@
+package com.onetwostory;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.UUID;
+
+@Path("/hello")
+public class Greeter {
+
+    private static final String EXAMPLE_TOKEN = "a6dfsdf8gfdg";
+    private static final String AUTHOR_NAME = "Ilya";
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String addHello(@HeaderParam("token") String token, @QueryParam("name") String aName, String body) {
+        if (EXAMPLE_TOKEN.equals(token))
+            return "success: author name is " + AUTHOR_NAME;
+        else return "fail";
+    }
+
+    @GET
+    @Path("{name}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String greetSomeone(@PathParam("name") String name) {
+        return "Hello, " + name;
+    }
+
+
+}
---
+++ src/test/java/com/onetwostory/NativeGreeterIT.java
@@ -0,0 +1,9 @@
+package com.onetwostory;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreeterIT extends GreeterTest {
+
+    // Execute the same tests but in native mode.
+}
